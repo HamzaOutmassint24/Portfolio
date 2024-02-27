b4f29a2c4960fe2d434e2fbfbb92cfd8df5ee55d
Hamza Outmassint <hamzaayrom@gmail.com>
2024-02-27 23:57:54 +0100

    few changes

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -19,7 +19,7 @@ export const ProjectSection = () => {
           PORTFOLIO
         </div>
       </div>
-      <div className="mt-9 grid grid-cols-1 lg:grid-cols-2 sm:place-items-center lg:place-items-start gap-8 lg:w-[84%] relative font-second-font">
+      <div className="my-24 grid grid-cols-1 lg:grid-cols-2 sm:place-items-center lg:place-items-start gap-8 lg:w-[84%] relative font-second-font">
         <div class="lg:max-w-sm sm:max-w-sm md:max-w-xl lg:h-72 rounded-lg overflow-hidden shadow-lg project-cart1">
           <div class="font-bold text-bg-color text-lg my-1 pl-2">Stock Building Materials</div>
           <img class="w-full px-2 cursor-pointer" src={Project1Img1} alt="Sunset in the mountains" onClick={handleModalToggle}/>
@@ -29,7 +29,7 @@ export const ProjectSection = () => {
               <span class="inline-block bg-bg-color rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#TailwindCss</span>
             </div>
             <span class="inline-block pl-3 py-1 mr-1 mb-2">
-              <a title="github" href="https://github.com/HamzaOutmassint" className="group">
+              <a title="github" href="https://github.com/HamzaOutmassint/Stock-Building-materials-V2" className="group">
                 <Github size={30} className="text-bg-color"/>
               </a>
             </span>          
@@ -44,7 +44,7 @@ export const ProjectSection = () => {
               <span class="inline-block bg-bg-color text-white rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Material UI</span>
             </div>
             <span class="inline-block pl-3 py-1 mr-1 mb-2">
-              <a title="github" href="https://github.com/HamzaOutmassint" className="group">
+              <a title="github" href="https://github.com/HamzaOutmassint/MultiShop" className="group">
                 <Github size={30} className="text-bg-color"/>
               </a>
             </span>
@@ -54,7 +54,7 @@ export const ProjectSection = () => {
 
       {isModalOpen && (
         <div onClick={handleModalToggle} className="fixed inset-0 bg-bg-color bg-opacity-50 flex items-center justify-center">
-          <div className="flex justify-center items-center project-cart1 text-bg-color p-8 relative rounded-lg w-[70%] h-[70%]">
+          <div className="flex justify-center items-center project-cart1 text-bg-color md:p-8 relative rounded-lg lg:w-[70%] lg:h-[70%] sm:w-[89%] sm:h-[37%] exactly-1024:h-[37%]">
             <img src={Project1Img1} alt="project image" className="rounded-md w-3/4"/>
             <button onClick={handleModalToggle} className='absolute top-4 right-4'>
               <XSquare size={18}/>
