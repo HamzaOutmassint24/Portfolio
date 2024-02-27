6c629682049b120bcdbbd9a7cef8bf5daa839308
Hamza Outmassint <hamzaayrom@gmail.com>
2024-02-27 23:28:15 +0100

    i finished from the project section

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -2,7 +2,7 @@ import { Envelope } from "react-bootstrap-icons"
 
 export const ContactSection = () => {
   return (
-    <section className="xl:h-full lg:h-screen exactly-1024:h-fit pt-9 pb-28 lg:w-[87%]" id="contact-section">
+    <section className="xl:h-full lg:h-full exactly-1024:h-fit pt-9 pb-28" id="contact-section">
       <div className="flex items-center md:justify-center sm:justify-start">
           <div className="border-1 border-second-color items-center rounded-full text-sm p-1 px-4 font-second-font tracking-normal sm:w-sm  md:w-md lg:w-lg xl:w-xl flex justify-between">
               <Envelope size={18}/>
